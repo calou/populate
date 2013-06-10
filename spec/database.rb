0679bfde31493efa757eb01b7066d1de013dd76b
Sébastien Gruchet <gruchet@gmail.com>
2013-06-11 01:50:24 +0200

    Moved database related class to an individual file

---
+++ spec/database.rb
@@ -0,0 +1,32 @@
+class Database
+  PROPERTIES = {
+    adapter:  'sqlite3',
+    database: File.expand_path("test.sqlite3", File.dirname(__FILE__))
+  }
+  def self.create
+    SQLite3::Database.new(PROPERTIES[:database]) do |db|
+      db.execute <<-SQL
+        create table dummy_classes (
+          id int(11),
+          p1 varchar(30),
+          p2 date
+        );
+      SQL
+      db.execute <<-SQL
+        create table timestamp_dummy_classes (
+          id int(11),
+          p1 varchar(30),
+          p2 date,
+          created_at datetime,
+          updated_at datetime
+        );
+      
+      SQL
+    end
+    ActiveRecord::Base.establish_connection PROPERTIES
+  end
+
+  def self.delete
+    File.delete(PROPERTIES[:database])
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,10 @@
 require 'populate'
 require 'active_record'
 require 'sqlite3'
+require 'database'
 require 'dummy_classes'
 
-class Database
-  PROPERTIES = {
-    adapter:  'sqlite3',
-    database: File.expand_path("test.sqlite3", File.dirname(__FILE__))
-  }
-  def self.create
-    SQLite3::Database.new(PROPERTIES[:database]) do |db|
-      db.execute <<-SQL
-        create table dummy_classes (
-          id int(11),
-          p1 varchar(30),
-          p2 date
-        );
-      SQL
-      db.execute <<-SQL
-        create table timestamp_dummy_classes (
-          id int(11),
-          p1 varchar(30),
-          p2 date,
-          created_at datetime,
-          updated_at datetime
-        );
-      
-      SQL
-    end
-    ActiveRecord::Base.establish_connection PROPERTIES
-  end
 
-  def self.delete
-    File.delete(PROPERTIES[:database])
-  end
-end
 
 RSpec.configure do |config|
   config.before(:suite) do
